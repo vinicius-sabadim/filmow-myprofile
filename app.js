6337b3a02363a55c71259bcc99ae4d20a68979bf
vinicius-sabadim <vinicius.sabadim@gmail.com>
2019-12-02 15:31:49 -0300

    Moving to an express server

--- app.js
+++ app.js
@@ -1,59 +1,17 @@
-import axios from 'axios'
-import cheerio from 'cheerio'
-import fs from 'fs'
+import express from 'express'
+import { getMoviesForUser, readMoviesFromFile } from './scrape'
 
-const user = process.argv[2]
-if (!user) {
-  console.error('Please inform an user.')
-  process.exit()
-}
+const app = express()
+const PORT = process.env.PORT || 3000
 
-async function getHTML(url) {
-  const { data: html } = await axios.get(url)
-  return html
-}
+app.get('/:user', async (req, res) => {
+  const movies = await getMoviesForUser(req.params.user)
+  res.json(movies)
+})
 
-async function run() {
-  let page = 1
-  let hasAnotherPage = true
-  const movies = []
+app.get('/movies/:user', async (req, res) => {
+  const movies = await readMoviesFromFile(req.params.user)
+  res.json(movies)
+})
 
-  while (hasAnotherPage) {
-    try {
-      const url = `https://filmow.com/usuario/${user}/filmes/ja-vi/?pagina=${page}`
-      const html = await getHTML(url)
-      const $ = cheerio.load(html)
-
-      console.log(`Fetching movies for the page ${page}...`)
-
-      $('.movie_list_item').each(async function() {
-        const title = $(this)
-          .find('.title')
-          .text()
-        const rating = $(this)
-          .find('.star-rating')
-          .attr('title')
-
-        const re = /Nota: (.+) estrelas?/
-
-        const movie = {
-          title,
-          rating: rating ? rating.replace(re, '$1') : null
-        }
-
-        movies.push(movie)
-      })
-
-      page = page + 1
-    } catch (err) {
-      hasAnotherPage = false
-    }
-  }
-  writeOnFile(movies)
-}
-
-const writeOnFile = movies => {
-  fs.writeFileSync('movies.json', JSON.stringify(movies))
-}
-
-run()
+app.listen(PORT, () => console.log(`Server listening on http://localhost:3000`))
---
+++ scrape.js
@@ -0,0 +1,59 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+import fs from 'fs'
+
+function writeOnFile(user, movies) {
+  fs.writeFileSync(`movies/${user}.json`, JSON.stringify(movies))
+}
+
+export async function readMoviesFromFile(user) {
+  const file = fs.readFileSync(`movies/${user}.json`)
+  const movies = JSON.parse(file)
+  return movies
+}
+
+async function getHTML(url) {
+  const { data: html } = await axios.get(url)
+  return html
+}
+
+export async function getMoviesForUser(user) {
+  let page = 1
+  let hasAnotherPage = true
+  const movies = []
+
+  while (hasAnotherPage) {
+    try {
+      const url = `https://filmow.com/usuario/${user}/filmes/ja-vi/?pagina=${page}`
+      const html = await getHTML(url)
+      const $ = cheerio.load(html)
+
+      console.log(`Fetching movies from ${user} - Page ${page}`)
+
+      $('.movie_list_item').each(async function() {
+        const title = $(this)
+          .find('.title')
+          .text()
+        const rating = $(this)
+          .find('.star-rating')
+          .attr('title')
+
+        const re = /Nota: (.+) estrelas?/
+
+        const movie = {
+          title,
+          rating: rating ? rating.replace(re, '$1') : null
+        }
+
+        movies.push(movie)
+      })
+
+      page = page + 1
+    } catch (err) {
+      console.error(err.message)
+      hasAnotherPage = false
+    }
+  }
+  writeOnFile(user, movies)
+  return movies
+}
